4e0f142b35d00dcfb72faf89aa55b43d18e94717
Elian M. Toribio <elianmtoribio@gmail.com>
2021-06-17 15:45:00 -0400

    barra navegacion procesos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraNavegacionesComponent } from './barra-navegaciones/barra-navegaciones.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraNavegacionesComponent
   ],
   imports: [
     BrowserModule,
