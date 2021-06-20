0901d609ea02dd496c2b6a24e5458d47e49f7a25
elianmtg <elianmtoribio@gmail.com>
2021-06-19 20:14:25 -0400

    barra nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BarraNavegacionesComponent } from './barra-navegaciones/barra-navegaciones.component';
 
 import { LoginComponent } from './vista/login/login.component';
 import { RegistroComponent } from './vista/registro/registro.component';
@@ -7,7 +8,8 @@ import { RegistroComponent } from './vista/registro/registro.component';
 const routes: Routes = [
   { path: '', redirectTo: 'Login', pathMatch: 'full' },
   { path: "Login", component: LoginComponent},
-  { path: "Registro", component: RegistroComponent}
+  { path: "Registro", component: RegistroComponent},
+  {path: "Navegacion", component: BarraNavegacionesComponent}
 ];
 
 @NgModule({
