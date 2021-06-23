56c191c003f6532d15f747e186348c2b0ec7226f
Junior Samuel De Los Santos Velazquez <juniorvelazquezmt19@gmail.com>
2021-06-22 22:25:33 -0400

    Prueba

--- src/app/vista/agregarcategoria/agregarcategoria.component.ts
+++ src/app/vista/agregarcategoria/agregarcategoria.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+
+
+export interface ICategoria {
+    idCategoria: number;
+    nombre: string;
+ }
+
+ const Datos: ICategoria[] = [
+   {idCategoria: 1, nombre: "Administrador"},
+   {idCategoria: 2, nombre: "Usuario"},
+   {idCategoria: 3, nombre: "Poster"}
+ ]
 
 @Component({
   selector: 'app-agregarcategoria',
@@ -7,9 +21,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarcategoriaComponent implements OnInit {
 
+   //Table
+   displayedColumns: string[] = ['Nombre'];
+   dataSource = new MatTableDataSource<ICategoria>(Datos);
+   
+   // MatPaginator Inputs
+   length = 100;
+   pageSize = 10;
+   pageSizeOptions: number[] = [5, 10, 25, 100];
+
   constructor() { }
 
+  @ViewChild(MatPaginator)  paginator!: MatPaginator;
+  
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator
+  }
+  
   ngOnInit(): void {
   }
 
+  pageEvent: PageEvent = new PageEvent;
+  setPageSizeOptions(setPageSizeOptionsInput: string) {
+    if (setPageSizeOptionsInput) {
+      this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
+    }
+  }
 }
