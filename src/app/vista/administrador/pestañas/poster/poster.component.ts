cbd1e2f0c93decbc5c7142639af79802d6fe69a4
elianmtg <elianmtoribio@gmail.com>
2021-07-01 18:29:31 -0400

    alertas listas todas

--- src/app/vista/administrador/pestañas/poster/poster.component.ts
+++ src/app/vista/administrador/pestañas/poster/poster.component.ts
@@ -8,6 +8,8 @@ import { AdministradorComponent } from '../../administrador.component';
 import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
 import { DatosService } from 'src/app/servicios/cargar/datos.service';
 
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-poster',
   templateUrl: './poster.component.html',
@@ -74,7 +76,32 @@ export class PosterComponent implements OnInit {
   }
 
   eliminar(id: number){
-    this.datos.deleteUsuario(id);
+    Swal.fire({
+      title: 'Esta seguro que desea eliminarlo?',
+      text: "No podra revertir los cambios!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, eliminala!',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        console.log('Selecciono elimiar el ', id);
+        this.datos.deleteUsuario(id).subscribe( x => {
+          this.datos.getUsuariosApi();
+          this.datos.getUsuario();
+
+        }, (err: any) => {
+          console.error(err);
+        });
+        Swal.fire(
+          'Eliminado!',
+          'Ha sido eliminado.',
+          'success'
+        )
+      }
+    })
   }
 
   editar(usuario: IUsuario){
