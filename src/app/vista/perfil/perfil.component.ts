1dcfba84cb5c0aa280f1f8dd00775c528357a448
elianmtg <elianmtoribio@gmail.com>
2021-07-03 13:32:14 -0400

    registro y mis post arreglados

--- src/app/vista/perfil/perfil.component.ts
+++ src/app/vista/perfil/perfil.component.ts
@@ -15,7 +15,8 @@ export class PerfilComponent implements OnInit {
     this.usuario = {
       idUsuario: parseInt(this.cookieS.get('ID')),
       nombre: cookieS.get('nombre'), 
-      apellido: 'No carga', 
+      // apellido: 'No carga', 
+      apellido: ('apellido'), 
       telefono: parseInt(this.cookieS.get('telefono')),
       cedula: parseInt(this.cookieS.get('cedula')),
       idRol: parseInt(this.cookieS.get('rol')),
--- src/app/vista/registrar/registrar.component.ts
+++ src/app/vista/registrar/registrar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormGroupName, Validators, FormBuilder } from '@angular/forms';
 import { MatDialogRef} from '@angular/material/dialog';
+import { IUsuario } from 'src/app/modelo/usuario';
+import { DatosService } from 'src/app/servicios/cargar/datos.service';
 
 import Swal from 'sweetalert2';
 
@@ -11,21 +13,60 @@ import Swal from 'sweetalert2';
 })
 export class RegistrarComponent implements OnInit {
 
+  habilitar: boolean = true;
+  usuario: IUsuario | undefined;
+  contrasena: string = '';  
+
   formRegis = this.formBuilder.group({
     nombre: ['',Validators.required],
     apellido: ['',Validators.required],
-    cedula: ['', Validators.required, [Validators.min(40000000000), Validators.max(40000000002)]],
-    telefono: ['',[Validators.required, Validators.min(8999999999), Validators.max(9000000002)]],
+    cedula: ['', Validators.required],
+    telefono: ['',Validators.required],
     correo: ['', [Validators.required, Validators.email]],
     contrasena1: ['',[Validators.required, Validators.minLength(8)]],
-    contrasena2: ['', [Validators.required, Validators.minLength(8)]]
+    contrasena2: ['', [Validators.required, Validators.minLength(8)]],
+    rol: ['',Validators.required]
   });
 
-  constructor(private formBuilder: FormBuilder, public dialogRef: MatDialogRef<RegistrarComponent>) { }
+  constructor(private formBuilder: FormBuilder, public dialogRef: MatDialogRef<RegistrarComponent>,private _dato: DatosService,) { }
 
   ngOnInit(): void {
   }
+  onSubmit(){
+    if (this.formRegis.value.contrasena1 == this.formRegis.value.contrasena2) {
 
+      this.usuario = {
+        idUsuario: 0,
+        nombre: this.formRegis.value.nombre,
+        apellido: this.formRegis.value.apellido,
+        idRol: this.formRegis.value.rol,
+        cedula: this.formRegis.value.cedula,
+        telefono: this.formRegis.value.telefono,
+        correo: this.formRegis.value.correo,
+        contrasena: this.contrasena
+      }
+      this._dato.postUsuario(this.usuario);
+      this.onClickNo();
+      Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Ha sido Registrado.',
+        showConfirmButton: false,
+        timer: 2500
+        
+      })
+    }else{
+      // alert('Contraseña no coinciden');
+      Swal.fire({
+        position: 'top-end',
+        icon: 'error',
+        title: 'Contraseñas no coinciden!',
+        text: 'Deben coincidir las contraseñas.',
+        showConfirmButton: false,
+        timer: 2000
+      })
+    }
+  }
   onClickNo(): void {
     this.dialogRef.close();
   }
